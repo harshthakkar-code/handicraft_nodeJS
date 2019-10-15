ad602bf6dcdadb085af6966f4d0c486c3b0e58c0
harshthakkar-code <harryparkar111@gmail.com>
2019-10-15 19:35:17 +0530

    display data in menu and delete done

--- public/models/customer_tbl_model.js
+++ public/models/customer_tbl_model.js
@@ -1,13 +1,13 @@
 var db=require('../dbconnection');
 var customer={
     getAllCustomer:function(callback){
-        return db.query("select *from customer_tb",callback);
+        return db.query("select * from customer_tb",callback);
     },
     addCustomer:function(item,callback){
-        return db.query("insert in customer_tb values(?,?,?,?,?,?)",[item.email, item.cust_name, item.ph_no, item.address, item.state, item.pincode],callback);
+        return db.query("insert into customer_tb values(?,?,?,?,?,?)",[item.email, item.cust_name, item.ph_no, item.address, item.state, item.pincode],callback);
     },
     deleteCustomer:function(email,callback){
-        return db.query("delete from customer_tb where email=?",email,callback);
+        return db.query("delete from customer_tb where email=?",[email],callback);
     }
 }
 module.exports=customer;
